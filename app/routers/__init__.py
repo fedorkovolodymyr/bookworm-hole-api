50893a6a19aadceb0134e9ee4a875a18e44a1905
Fedorko Volodymyr <fedorkovol@gmail.com>
2026-07-05 19:10:13 +0300

    feat(statuses): add aggregated /me/library, /me/wishlist, /me/lent-out, /me/borrowed views (#124)

--- app/routers/__init__.py
+++ app/routers/__init__.py
@@ -9,6 +9,7 @@ from app.routers.friends import friends_router
 from app.routers.health import health_router
 from app.routers.releases import releases_router
 from app.routers.reviews import reviews_router
+from app.routers.status_views import status_views_router
 from app.routers.statuses import statuses_router
 from app.routers.users import users_router
 
@@ -20,6 +21,7 @@ api_v1.include_router(releases_router)
 api_v1.include_router(contributors_router)
 api_v1.include_router(auth_router)
 api_v1.include_router(statuses_router)
+api_v1.include_router(status_views_router)
 api_v1.include_router(collections_router)
 api_v1.include_router(external_router)
 api_v1.include_router(friends_router)
